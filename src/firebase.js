02e0e22a3e2dde2931c67f85739fc76cfada9e0b
simplyDonald <donaldabuah@gmail.com>
2022-12-11 23:31:28 -0500

    add firebase setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+import firebase from "firebase";
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyCis9wXjmg0lRNNptEkkj6glI920XnYpSA",
   authDomain: "slack-clone-c6db2.firebaseapp.com",
@@ -6,3 +9,11 @@ const firebaseConfig = {
   messagingSenderId: "287766835147",
   appId: "1:287766835147:web:dda6ecb1dcbbdd95ff904e"
 };
+
+const app = firebase.initializeApp(firebaseConfig);
+
+const db = app.firestore();
+
+const auth = firebase.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();

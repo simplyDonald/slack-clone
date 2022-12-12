8cf25bb781e7be76b258a75a44edc1f9173f15dc
simplyDonald <donaldabuah@gmail.com>
2022-12-11 22:01:26 -0500

    style header and add styled Appbody comp. with sidebar comp

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Header from './components/Header';
+import styled from "styled-components";
 
 
 function App() {
@@ -9,12 +10,14 @@ function App() {
     <div className="app">
       <BrowserRouter>
       <Header />
-
+      <AppBody>
+        <Sidebar />
         <Routes>
           <Route path="/" element={<div></div>} />
           <Route path="/about" element={<div>About</div>} />
           <Route path="*" element={<div>Hi</div>} />
         </Routes>
+      </AppBody>
         
       </BrowserRouter>
     </div>
@@ -22,3 +25,6 @@ function App() {
 }
 
 export default App;
+
+const AppBody = styled.div`
+`
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ import React from 'react'
 import styled from "styled-components"
 import Avatar from '@mui/material/Avatar';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import SearchIcon from '@mui/icons-material/Search';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 
 function Header() {
   return (
@@ -14,18 +16,82 @@ function Header() {
         <AccessTimeIcon />
       </HeaderLeft>
       {/* Header Search */}
+      <HeaderSearch>
+        <SearchIcon />
+        <input placeholder="Search" />
+      </HeaderSearch>
       {/* Header right */}
+      <HeaderRight>
+        <HelpOutlineIcon />
+      </HeaderRight>
     </HeaderContainer>
   )
 }
 
 export default Header;
 
+const HeaderRight = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: flex-end;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 20px;
+  }
+`
+
+const HeaderSearch = styled.div`
+  flex: 0.4;
+  opacity: 1;
+  border-radius: 6px;
+  background-color: #421f44;
+  text-align: center;
+  display: flex;
+  padding: 0 50px;
+  color: gray;
+  border: 1px gray solid;
+
+  > input {
+    background-color: transparent;
+    border: none;
+    text-align: center;
+    min-width: 30vw;
+    outline: 0;
+    color: white;
+  }
+
+`
+
 const HeaderContainer = styled.div`
-  color: red;
+display: flex;
+position: fixed;
+width: 100%;
+align-items: center;
+justify-content: space-between;
+padding: 10px 0;
+background-color: var(--slack-color);
+color: white;
+
 `
 ;
 
-const HeaderLeft = styled.div``;
+const HeaderLeft = styled.div`
+  display: flex;
+  flex: 0.3;
+  align-items: center;
+  margin-left: 20px;
 
-const HeaderAvatar = styled(Avatar)``;
+  >.MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 30px;
+  }
+`;
+
+const HeaderAvatar = styled(Avatar)`
+  cursor: pointer;
+
+    :hover {
+      opacity: 0.8;
+    }
+`;

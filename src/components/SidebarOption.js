852965278d837e9968b457759054c511355489a2
simplyDonald <donaldabuah@gmail.com>
2022-12-12 18:11:45 -0500

    setup select function to dispatch id to redux store

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
 import styled from "styled-components"
+import { enterRoom } from '../features/appSlice';
 import {db} from "../firebase"
 
-function SidebarOption({Icon, title, addChannelOption}) {
+function SidebarOption({Icon, title, addChannelOption, id}) {
  
+  const dispatch = useDispatch();
 
   const addChannel = () => {
     const channelName = prompt("Please enter the channel name");
@@ -18,7 +21,11 @@ function SidebarOption({Icon, title, addChannelOption}) {
   }
 
   const selectChannel = () => {
-
+    if (id) {
+      dispatch(enterRoom({
+        roomId: id
+      }));
+    }
   }
   return (
     <SidebarOptionContainer onClick={addChannelOption ? addChannel : selectChannel}>

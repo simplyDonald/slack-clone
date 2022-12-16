a7770adfa5cfae7f62659ce661b8eb13c50e55a6
simplyDonald <donaldabuah@gmail.com>
2022-12-16 16:43:08 -0500

    implement user login feature

--- src/App.js
+++ src/App.js
@@ -5,23 +5,30 @@ import Header from './components/Header';
 import styled from "styled-components";
 import Sidebar from './components/Sidebar';
 import Chat from './components/Chat';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import {auth} from './firebase';
+import Login from './components/Login';
 
 
 function App() {
+  const [user, loading] = useAuthState(auth);
   return (
     <div className="app">
-      <BrowserRouter>
-      <Header />
-      <AppBody>
-        <Sidebar />
-        <Routes>
-          <Route path="/" element={<Chat />} />
-          <Route path="/about" element={<div>About</div>} />
-          <Route path="*" element={<div>Hi</div>} />
-        </Routes>
-      </AppBody>
+      {user ? (
+        <BrowserRouter>
+          <Header />
+          <AppBody>
+            <Sidebar />
+            <Routes>
+              <Route path="/" element={<Chat />} />
+              <Route path="/about" element={<div>About</div>} />
+              <Route path="*" element={<div>Hi</div>} />
+            </Routes>
+          </AppBody>
         
-      </BrowserRouter>
+        </BrowserRouter>
+      ) : <Login />}
+      
     </div>
   );
 }

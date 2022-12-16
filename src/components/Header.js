1095add73b35f2ea753642b5b1bfedcb2e798fd8
simplyDonald <donaldabuah@gmail.com>
2022-12-16 16:58:21 -0500

    setup dynamic profile pic

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,18 @@ import Avatar from '@mui/material/Avatar';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import SearchIcon from '@mui/icons-material/Search';
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../firebase';
 
 function Header() {
+  const [user] = useAuthState(auth);
   return (
     <HeaderContainer>
       {/* Header left */}
       <HeaderLeft>
         <HeaderAvatar 
-          //TODO: Add onClick
+          src={user?.photoURL}
+          onClick={() => auth.signOut()}
         />
         <AccessTimeIcon />
       </HeaderLeft>

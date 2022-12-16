e44fee84e18df91b24b3394f8fc1d41cea5e4f1f
simplyDonald <donaldabuah@gmail.com>
2022-12-16 17:42:02 -0500

    add spinkit pkg

--- src/App.js
+++ src/App.js
@@ -8,10 +8,30 @@ import Chat from './components/Chat';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import {auth} from './firebase';
 import Login from './components/Login';
+import Spinner from "react-spinkit";
 
 
 function App() {
   const [user, loading] = useAuthState(auth);
+  
+  if (loading) {
+    return (
+      <AppLoading>
+        <AppLoadingContents>
+          <img src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg" alt="" />
+
+          <Spinner 
+          name="cube-grid"
+          color="purple"
+          fadeIn="none"
+          />
+            
+        </AppLoadingContents>
+      </AppLoading>
+    )
+
+
+  }
   return (
     <div className="app">
       {user ? (
@@ -39,4 +59,28 @@ const AppBody = styled.div`
   display: flex;
   height: 100vh;
   
+`
+
+const AppLoading = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100%;
+
+`
+
+const AppLoadingContents = styled.div`
+  
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: 100vh;
+  
+  > img {
+    height: 100px;
+    padding: 20px;
+    margin-bottom: 40px;
+  }
 `

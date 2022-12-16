28acfff2ec218e82c113c9ad1132e2e99fcd8fe0
simplyDonald <donaldabuah@gmail.com>
2022-12-16 17:40:49 -0500

    setup dynamic username

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -15,12 +15,17 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import AddIcon from '@mui/icons-material/Add';
 import {db} from "../firebase"
 import {useCollection} from "react-firebase-hooks/firestore"
+import {useAuthState} from "react-firebase-hooks/auth"
+import {auth} from "../firebase"
 
 
 
 function Sidebar() {
   // get all the channels from db
   const [channels, loading, error] = useCollection(db.collection("rooms"))
+
+  // get the user
+  const [user] = useAuthState(auth)
 
   return (
     <SidebarContainer>
@@ -29,7 +34,7 @@ function Sidebar() {
           <h2>Coders' Box</h2>
           <h3>
             <FiberManualRecordIcon />
-            User Name
+            {user?.displayName}
           </h3>
         </SidebarInfo>
         <CreateIcon />
